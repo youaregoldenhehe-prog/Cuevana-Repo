54f7f29d3bff38f36c086bd0e7a9e08f299bae14
youaregoldenhehe-prog <youaregoldenhehe@gmail.com>
2026-04-14 16:37:49 +0200

    Create build.gradle.kts

---
+++ Cuevana/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    authors = listOf("youaregoldenhehe")
+    language = "es"
+    description = "Cuevana Plugin"
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+}
